832bc8bb7babea98bd327d4ed5a981d2e921302f
Mathias Nielsen <MathiasWael@hotmail.dk>
2020-05-06 10:41:45 +0200

    Controller Setup

---
+++ CookingBook/CookingBook/CookingBook/Controller/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using CookingBook.Data;
+using CookingBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookingBook.Controller
+{
+    public class RecipeController
+    {
+        private DataManager dataManager;
+
+        public RecipeController()
+        {
+            dataManager = new DataManager();
+        }
+
+        public async System.Threading.Tasks.Task<List<Recipe>> GetRecipesAsync(int number, int offset)
+        {
+            return await dataManager.GetRecipesAsync(number, offset);
+        }
+    }
+}
