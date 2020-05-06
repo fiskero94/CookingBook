6ed9888bf3c3144ee467a2189cf41c004af1d9c2
Mathias Nielsen <MathiasWael@hotmail.dk>
2020-05-06 10:36:21 +0200

    API Location

--- CookingBook/CookingBook/CookingBook/Data/DataManager.cs
+++ CookingBook/CookingBook/CookingBook/Data/DataManager.cs
@@ -7,19 +7,19 @@ using System.Text;
 
 namespace CookingBook
 {
-    public class API
+    public class DataManager
     {
         private HttpClient Client;
         private string Key = "24d41fab91e74391b6acb1cee3c86983";
 
-        public API()
+        public DataManager()
         {
             Client = new HttpClient();
         }
 
-        public async System.Threading.Tasks.Task<List<Recipe>> GetRecipesAsync()
+        public async System.Threading.Tasks.Task<List<Recipe>> GetRecipesAsync(int number, int offset)
         {
-            HttpResponseMessage response = await Client.GetAsync("https://api.spoonacular.com/recipes/search?apiKey=" + Key);
+            HttpResponseMessage response = await Client.GetAsync("https://api.spoonacular.com/recipes/search?apiKey=" + Key + "&number=" + number + "&offset=" + offset);
             string content = await response.Content.ReadAsStringAsync();
             RecipeResults results = JsonConvert.DeserializeObject<RecipeResults>(content);
             HttpResponseMessage response2 = await Client.GetAsync("https://api.spoonacular.com/recipes/informationBulk?ids=" + results.Ids + "&apiKey=" + Key);
